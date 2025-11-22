abca6aa7242aa8014e69952dedeae6ed19833962
Alex Castro <git@alexcastro.dev>
2025-11-22 15:01:43 -0300

    Standardizes code indentation to 2 spaces
    Improves readability and maintains consistent formatting by converting all indentation from tabs to 2 spaces throughout the file. No logic changes introduced.

--- src/index.ts
+++ src/index.ts
@@ -5,99 +5,99 @@ import { EXIT, visit } from 'unist-util-visit';
 
 
 export type RemarkShikiOptions = {
-	highlighterOptions: BundledHighlighterOptions<BundledLanguage, BundledTheme>,
-	inferLang?: ((snippet: string) => Promise<Lang | undefined>) | string,
-	codeToHtmlOptions: CodeToHastOptions<ResolveBundleKey<Lang>, ResolveBundleKey<BundledTheme>>
+  highlighterOptions: BundledHighlighterOptions<BundledLanguage, BundledTheme>,
+  inferLang?: ((snippet: string) => Promise<Lang | undefined>) | string,
+  codeToHtmlOptions: CodeToHastOptions<ResolveBundleKey<Lang>, ResolveBundleKey<BundledTheme>>
 }
 
 export interface RemarkNode extends Parent {
-	type: string;
-	value: string;
-	lang?: Lang;
+  type: string
+  value: string
+  lang?: Lang
 }
 
 export default async function remarkShiki({ markdownAST }: any, options: RemarkShikiOptions) {
-	await recursivelyFindAndHighlightNextCodeNode(markdownAST, options)
-	return markdownAST
+  await recursivelyFindAndHighlightNextCodeNode(markdownAST, options)
+  return markdownAST
 }
 
 async function recursivelyFindAndHighlightNextCodeNode(markdownAST: any, options: RemarkShikiOptions) {
-	const node = findNextCodeNodeFromMarkdownAST(markdownAST)
+  const node = findNextCodeNodeFromMarkdownAST(markdownAST)
 
-	if (node) {
-		await highlightCodeNode(node, options)
-		await recursivelyFindAndHighlightNextCodeNode(markdownAST, options)
-	}
+  if (node) {
+    await highlightCodeNode(node, options)
+    await recursivelyFindAndHighlightNextCodeNode(markdownAST, options)
+  }
 }
 
 async function highlightCodeNode(node: RemarkNode, options: RemarkShikiOptions) {
-	node.type = `html`
-	node.children = []
-	node.value = await getNodeHighlightedHtml({ codeSnippet: node.value, explicitLang: node.lang }, options)
+  node.type = `html`
+  node.children = []
+  node.value = await getNodeHighlightedHtml({ codeSnippet: node.value, explicitLang: node.lang }, options)
 }
 
 export type CodeSnippetNodeData = {
-	explicitLang?: string
-	codeSnippet: string
+  explicitLang?: string
+  codeSnippet: string
 }
 
 async function getNodeHighlightedHtml({ explicitLang, codeSnippet }: CodeSnippetNodeData, options: RemarkShikiOptions): Promise<string> {
-	const { highlighterOptions, codeToHtmlOptions, inferLang } = options
+  const { highlighterOptions, codeToHtmlOptions, inferLang } = options
 
-	const highlighter = highlighterOptions ? await createHighlighter(highlighterOptions) : undefined;
+  const highlighter = highlighterOptions ? await createHighlighter(highlighterOptions) : undefined;
 
-	const inferredLang = await (async () => {
-		if (explicitLang) {
-			return undefined
-		}
+  const inferredLang = await (async () => {
+    if (explicitLang) {
+      return undefined
+    }
 
-		if (typeof inferLang === `function`) {
-			return await inferLang(codeSnippet)
-		}
+    if (typeof inferLang === `function`) {
+      return await inferLang(codeSnippet)
+    }
 
-		if (typeof inferLang === `string`) {
-			return inferLang as Lang
-		}
+    if (typeof inferLang === `string`) {
+      return inferLang as Lang
+    }
 
-		return undefined
-	})()
+    return undefined
+  })()
 
-	const lang = explicitLang ?? inferredLang
+  const lang = explicitLang ?? inferredLang
 
-	if (!lang) {
-		return `<pre class="shiki-unknown"><code>${codeSnippet}</code></pre>`;
-	}
+  if (!lang) {
+    return `<pre class="shiki-unknown"><code>${codeSnippet}</code></pre>`;
+  }
 
-	const internalTransformers = inferredLang ? [addInferredLangFlagToShikiPreElement] : []
-	const userTransformers = codeToHtmlOptions.transformers ?? []
+  const internalTransformers = inferredLang ? [addInferredLangFlagToShikiPreElement] : []
+  const userTransformers = codeToHtmlOptions.transformers ?? []
 
-	const transformers = [...userTransformers, ...internalTransformers]
+  const transformers = [...userTransformers, ...internalTransformers]
 
-	const effectiveCodeToHtmlOptions: typeof codeToHtmlOptions = {
-		...codeToHtmlOptions,
-		lang: lang,
-		transformers: transformers,
-	}
+  const effectiveCodeToHtmlOptions: typeof codeToHtmlOptions = {
+    ...codeToHtmlOptions,
+    lang: lang,
+    transformers: transformers,
+  }
 
-	if (highlighter) {
-		return highlighter.codeToHtml(codeSnippet, effectiveCodeToHtmlOptions);
-	}
+  if (highlighter) {
+    return highlighter.codeToHtml(codeSnippet, effectiveCodeToHtmlOptions);
+  }
 
-	return await codeToHtml(codeSnippet, effectiveCodeToHtmlOptions);
+  return await codeToHtml(codeSnippet, effectiveCodeToHtmlOptions);
 }
 
 function findNextCodeNodeFromMarkdownAST(markdownAST: any): RemarkNode | undefined {
-	let result: RemarkNode | undefined = undefined
-	visit(markdownAST, `code`, (node: RemarkNode) => {
-		result = node
-		return [EXIT]
-	});
-	return result
+  let result: RemarkNode | undefined = undefined
+  visit(markdownAST, `code`, (node: RemarkNode) => {
+    result = node
+    return [EXIT]
+  });
+  return result
 }
 
 const addInferredLangFlagToShikiPreElement: ShikiTransformer = {
-	pre: function (node) {
-		this.addClassToHast(node, `shiki-lang-was-inferred`)
-		node.properties[`data-shiki-lang-was-inferred`] = `1`
-	},
+  pre: function (node) {
+    this.addClassToHast(node, `shiki-lang-was-inferred`)
+    node.properties[`data-shiki-lang-was-inferred`] = `1`
+  },
 }
